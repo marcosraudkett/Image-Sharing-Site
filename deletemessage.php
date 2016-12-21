20c4ea5a9881db430478a620bb7dfdfafddf2c86
Marcos Raudkett <info@marcosraudkett.com>
2016-12-21 21:21:20 +0200

    Update deletemessage.php

--- deletemessage.php
+++ deletemessage.php
@@ -1,18 +1,14 @@
 <?php
-//Tällä scriptillä lisätään tietokantaan kommentti
 
-//lisätään yhteys tietokantaan
-include("../mvrclabs/uploads/8/0/4/6/8046813/tietokanta.php");
+include("db.php");
 
 // if ($_POST['submit']) {
 
-	//haetaan tiedot users.php sivulta
 	$message_id = $_POST["message_id"];
 
-	//poistetaan viesti tietokannasta
 	mysql_query("DELETE FROM messages WHERE message_id = '$message_id'");
 
-	//palautetaan takaisin viesteihin
+
 	header("Location: http://marcosraudkett.com/projektityo/messages.php");
 
 // } else {
