a7a852269ef1ca44b92f8063ae357289bb60d6c0
Marcos Raudkett <info@marcosraudkett.com>
2016-12-21 21:31:17 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php 
 //connetction to the database (change to whatever you'd like.)
-$conn = mysql_connect("localhost", "root", "") or die(mysql_error()); mysql_select_db("") or die(mysql_error()); 
+$conn = mysql_connect("localhost", "root", "") or die(mysql_error()); mysql_select_db("projektityo") or die(mysql_error()); 
 //charset to UTF8
 mysql_set_charset("utf8", $conn);
 
